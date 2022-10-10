ff2a003921311bd0029e395d9cc08dba61f780fb
Albert Volkman <albert.volkman@gmail.com>
2022-10-10 14:27:15 -0400

    Convert to bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -5,47 +5,81 @@ import htmlmin from 'gulp-htmlmin';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
-import uglify from 'gulp-uglify';
+import terser from 'gulp-terser';
 import postcss from 'gulp-postcss';
-import { postcssPlugin as uncss } from 'uncss';
+import purgecss from '@fullhuman/postcss-purgecss';
 import cssnano from 'cssnano';
+import removeComments from 'postcss-discard-comments';
 import { default as log } from 'fancy-log';
 
-// Compile SASS.
-export const css = () => gulp.src('./_css/townies.scss')
+// Dev
+export const cssDev = () => gulp.src('./_css/townies.scss')
     .pipe(sass({
       includePaths: [
         './_css',
-        './node_modules/foundation-sites/scss',
+        './node_modules/bootstrap/scss',
       ],
-      //outputStyle: 'compressed'
     })
     .on('error', sass.logError))
-    .pipe(postcss([uncss({
-        csspath: '_site/css/townies.css',
-        html: [
+    .pipe(gulp.dest('./css'));
+
+export const cssWatch = () => gulp.watch('./_css/**/*.scss', cssDev);
+
+export const jsDev = () => gulp.src([
+    './node_modules/bootstrap/dist/js/bootstrap.min.js'
+  ])
+  .pipe(gulp.dest('js'));
+
+export const jsWatch = () => gulp.watch('./_js/**/*.js', jsDev);
+
+export const jekyllServe = () => {
+  const jekyll = spawn('bundle', ['exec', 'jekyll', 'serve']);
+
+  const jekyllLogger = function(buffer) {
+    buffer.toString()
+      .split(/\n/)
+      .forEach(function(message) {
+        log('Jekyll: ' + message);
+      });
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+}
+
+export const dev = gulp.series(cssDev, gulp.parallel(jekyllServe, cssWatch));
+
+// Prod
+export const cssProd = () => gulp.src('./_css/townies.scss')
+    .pipe(sass({
+      includePaths: [
+        './_css',
+        './node_modules/bootstrap/scss',
+      ],
+    })
+    .on('error', sass.logError))
+    .pipe(postcss([purgecss({
+        content: [
           '_site/index.html',
           '_site/appointments.html',
           '_site/barbers.html',
-        ],
-      }), cssnano()]))
-    .pipe(gulp.dest('./css'));
+        ]
+      }),
+      removeComments({ removeAll: true }),
+      cssnano()
+    ]))
+    .pipe(gulp.dest('./_site/css'));
 
-export const cssWatch = () => gulp.watch('./_css/**/*.scss', ['css']);
-
-// Compile JS.
-export const js = () => gulp.src([
-    './node_modules/foundation-sites/dist/js/foundation.js',
+export const jsProd = () => gulp.src([
+    './node_modules/bootstrap/dist/js/bootstrap.min.js'
   ])
-    .pipe(concat('townies.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('js'));
-
-export const jsWatch = () => gulp.watch('./_js/**/*.js', ['js']);
-
-export const htmlMinify = () => gulp.src('_site/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('_site'));
+    .pipe(terser({
+      toplevel: true,
+      format: {
+        comments: false
+      }
+    }))
+    .pipe(gulp.dest('_site/js'));
 
 export const jekyll = (gulpCallback) => {
   const jekyll = spawn('bundle', ['exec', 'jekyll', 'build']);
@@ -64,23 +98,6 @@ export const jekyll = (gulpCallback) => {
   jekyll.on('exit', gulpCallback);
 }
 
-export const jekyllServe = () => {
-  const jekyll = spawn('bundle', ['exec', 'jekyll', 'serve']);
-
-  const jekyllLogger = function(buffer) {
-    buffer.toString()
-      .split(/\n/)
-      .forEach(function(message) {
-        log('Jekyll: ' + message);
-      });
-  };
-
-  jekyll.stdout.on('data', jekyllLogger);
-  jekyll.stderr.on('data', jekyllLogger);
-}
-
-export const dev = gulp.series(css, js, jekyllServe, cssWatch, jsWatch);
-
-export const build = gulp.series(css, js, jekyll, htmlMinify);
+export const build = gulp.series(jekyll, cssProd, jsProd);
 
 export default dev;
